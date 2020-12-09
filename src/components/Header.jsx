d10e0ff6f178268d421f05194fcfb2f227e61e1d
anubhav14g <anubhav1408sks@gmail.com>
2020-12-09 12:07:00 +0530

    front-end part is complete.No backend is used

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
+
+function Header() {
+  return (
+    <header>
+      <h1><AllInclusiveIcon />  Notes App</h1>
+    </header>
+  );
+}
+
+export default Header;
